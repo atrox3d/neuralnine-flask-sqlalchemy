4fbef8d5649173205d5bea76017ac23c892c18b8
atrox3d <atrox3d@gmail.com>
2024-07-22 11:13:31 +0200

    add exception

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+class FlaskAppAlreadyInitializedException(Exception):
+    pass
 
 class FlaskApp:
-    __initialized: bool = False
+    __instance: 'FlaskApp' = None
 
     def __init__(self, name:str, dbpath:str='data.db') -> None:
-        if self.__initialized:
-            raise ValueError(f'FlaskApp is already initialized')
+        if self.__instance:
+            raise FlaskAppAlreadyInitializedException(f'FlaskApp is already initialized')
         self.__name = name
         self.__dbpath = dbpath
         self.__app = Flask(self.__name)
         self.__app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{self.__dbpath}'
         self.__db = SQLAlchemy(self.__app)
-        FlaskApp.__initialized = True
+        FlaskApp.__instance = self
 
     def app(self):
         return self.__app
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask_app import FlaskApp
+from flask_app import FlaskApp, FlaskAppAlreadyInitializedException
 
 fa = FlaskApp(__name__)
-# fa = FlaskApp(__name__)
+try:
+    fa = FlaskApp(__name__)
+except FlaskAppAlreadyInitializedException as faaie:
+    print(faaie)
 db = fa.db()
 print(db)
 print(fa.app())
