e886d18f33b29d1f9955aad8da67132c2c672075
atrox3d <atrox3d@gmail.com>
2024-07-22 14:42:11 +0200

    add post insert person

--- routes.py
+++ routes.py
@@ -1,11 +1,27 @@
-from flask import jsonify
-import flask_factory
+from flask import jsonify, request
+import flask_factory as ff
 from models import Person
+from sqlalchemy import exc
 
-app = flask_factory.get_app()
+app = ff.get_app()
+db = ff.get_db()
 
-@app.route('/')
-def index():
+def get_people_json():
     people = Person.query.all()
     return jsonify([person.dict() for person in people])
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.json)
+        person = Person(**request.json)
+        try:
+            db.session.add(person)
+            db.session.commit()
+            return get_people_json()
+        except exc.IntegrityError as ie:
+            return jsonify({'error': str(ie)})
+
+    elif request.method == 'GET':
+        return get_people_json()
 
