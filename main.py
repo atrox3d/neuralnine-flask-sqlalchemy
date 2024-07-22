dee2ae09457750e76fc91e238a0ce720c8046d54
atrox3d <atrox3d@gmail.com>
2024-07-22 12:57:57 +0200

    create model

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import flask_factory
-import models
+from models import Person
 
 app = flask_factory.get_app()
+db = flask_factory.get_db()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
 
 app.run(debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,17 @@
 import flask_factory
+import sqlalchemy as sa         # this is really horrible, there is no intellisense with SQLAlchemy module
 
 db = flask_factory.get_db()
+
+class Person(db.Model):
+
+    __tablename__ = 'people'
+    
+    pid = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Text, nullable=False)
+    age = sa.Column(sa.Integer)
+    job = sa.Column(sa.Text)
+
+    def __repr__(self) -> str:
+        return f'{self.name} ({self.age}): {self.job}'
 
