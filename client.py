18e9b9ca78c3aab4480d37536da5e7fd417c937d
atrox3d <atrox3d@gmail.com>
2024-07-23 13:13:06 +0200

    implement client add people

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 import requests
 import json
 
+SERVER = 'http://127.0.0.1:5000'
+
 def list_people():
-    response = requests.get('http://127.0.0.1:5000/')
+    response = requests.get(f'{SERVER}/')
+    response.raise_for_status()
     print(json.dumps(response.json(), indent=2))
 
+def add_people(name, age, job):
+    payload = dict(name=name, age=age, job=job)
+    response = requests.post(f'{SERVER}/add', json=payload)
+    response.raise_for_status()
+    print(json.dumps(response.json()))
+
+
+add_people('brian', 12, 'cat')
 list_people()
