1276851510bb97a6df5a4fd321b2b0d597f0c115
atrox3d <atrox3d@gmail.com>
2024-07-23 14:32:47 +0200

    move routes.py to routes package

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask_migrate import Migrate
 
 import flask_factory
 from models import Person
-import routes
+import routes.routes as routes
 
 app = flask_factory.get_app()
 db = flask_factory.get_db()
