7f1be53942726cd6c9ea666ad9a91fb393133775
atrox3d <atrox3d@gmail.com>
2024-07-24 05:33:38 +0200

    add login manager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 import flask_factory
 # import routes.application as application
@@ -6,6 +8,15 @@ from routes import application, login
 
 app = flask_factory.get_app()
 db = flask_factory.get_db()
+
+app.secret_key = 'SECRETKEY'
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(uid):
+    return User.query.get(uid)
+
+# bcrypt =
 
 # with app.app_context():
     # db.drop_all()
