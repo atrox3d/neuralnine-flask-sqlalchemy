8d2441f8c61044fa641096641a095192d028fe8e
atrox3d <atrox3d@gmail.com>
2024-07-25 18:32:40 +0200

    application/resister_routes

--- routes/application.py
+++ routes/application.py
@@ -1,4 +1,5 @@
-from flask import jsonify, redirect, request, url_for
+from flask import Flask, jsonify, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 from models.person import Person
 from sqlalchemy import exc
 
@@ -7,38 +8,40 @@ from singleton_store import SingletonStore as store
 
 # app = ff.get_app()
 # db = ff.get_db()
-app = store.get('app')
-db = store.get('db')
+# app = store.get('app')
+# db = store.get('db')
 
-def get_people_json():
-    people = Person.query.all()
-    return jsonify([person.dict() for person in people])
+def register_routes(app:Flask, db:SQLAlchemy):
 
-@app.get('/')
-def index():
-    people = get_people_json()
-    # print(len(people.json))
-    return people
+    def get_people_json():
+        people = Person.query.all()
+        return jsonify([person.dict() for person in people])
 
-@app.post('/add')
-def add():
-    print(f'{request.json = }')
-    person = Person(**request.json)
-    try:
-        print(f'adding {person}')
-        db.session.add(person)
-        db.session.commit()
-        return get_people_json()
-    except exc.IntegrityError as ie:
-        return jsonify({'error': str(ie)})
+    @app.get('/')
+    def index():
+        people = get_people_json()
+        # print(len(people.json))
+        return people
 
-@app.delete('/delete')
-def delete():
-    print(request.json)
-    person:Person = Person.query.get(request.json['pid'])
-    if person:
-        db.session.delete(person)
-        db.session.commit()
-        return get_people_json()
-    else:
-        return jsonify({'error': f'cannot find {request.json["pid"]}'})
+    @app.post('/add')
+    def add():
+        print(f'{request.json = }')
+        person = Person(**request.json)
+        try:
+            print(f'adding {person}')
+            db.session.add(person)
+            db.session.commit()
+            return get_people_json()
+        except exc.IntegrityError as ie:
+            return jsonify({'error': str(ie)})
+
+    @app.delete('/delete')
+    def delete():
+        print(request.json)
+        person:Person = Person.query.get(request.json['pid'])
+        if person:
+            db.session.delete(person)
+            db.session.commit()
+            return get_people_json()
+        else:
+            return jsonify({'error': f'cannot find {request.json["pid"]}'})
