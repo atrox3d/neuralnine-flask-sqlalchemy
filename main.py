24c234043f096be2118b6e9ef6a7c04434f2ee36
atrox3d <atrox3d@gmail.com>
2024-07-25 10:01:42 +0200

    store all

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ from singleton_store import SingletonStore as store
 
 # app = flask_factory.get_app()
 # db = flask_factory.get_db()
+
 app = Flask(__name__)
+store.add('app', app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///data.db'
 db = SQLAlchemy(app)
-store.add_values(app=app, db=db)
-
+store.add('db', db)
 
 from routes import application, login
 
 app.secret_key = 'SECRETKEY'
 login_manager = LoginManager()
+store.add('login_manager', login_manager)
 login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(uid):
     return User.query.get(uid)
+
 
 # bcrypt =
 
