afdae3b468e97cd60bdfccee78d34d4a7bee2910
atrox3d <atrox3d@gmail.com>
2024-07-27 10:02:41 +0200

    test_add_people

--- tests/flask_test_client/test_people.py
+++ tests/flask_test_client/test_people.py
@@ -3,33 +3,32 @@ import main
 
 # from app.models import Stock
 from models.the_db import db
+from models.person import Person
 from main import setup_app
 
 class TestPeople:
 
     @classmethod
-    def setup_method(self):
-        # self.app = main.app
-        # self.db = main.db
-        # self.app.config["TESTING"] = True
-        # self.app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///test_data.db'
-        # self.db.init_app(self.app)
-        self.db = db
-        self.app = setup_app(__name__,
-                            self.db,
+    def setup_class(cls):
+        cls.db = db
+        print(f'SETUP_CLASS | setting up app')
+        cls.app = setup_app(__name__,
+                            cls.db,
                             SQLALCHEMY_DATABASE_URI=f'sqlite:///test_data.db',
                             SECRET_KEY='SECRETKEY'
                              )
 
         # print(self.app.config)
+        print(f'SETUP_CLASS | setting up test client')
+        with cls.app.test_client() as client:
+            cls.client = client
+
+    def setup_method(self):
+        print(f'SETUP_METOD | resetting test db')
         with self.app.app_context():
             self.db.drop_all()
             self.db.create_all()
 
-        with self.app.test_client() as client:
-            self.client = client
-
-    @classmethod
     def teardown_method(self):
         pass
     
@@ -44,8 +43,17 @@ class TestPeople:
         # print(self.app.url_map)
         # return
 
-    def test_add_people(): pass
-    
+    def test_add_people(self):
+        payload = dict(name='test', age=0, job='nojob')
+        response = self.client.post('/add', json=payload)
+        assert response.status_code == 200
+        assert len(response.json) == 1
+
+        person = Person(pid=1, **payload)
+        rperson = Person(**response.json[0])
+        assert rperson.dict() == person.dict()
+        assert repr(rperson) == repr(person)
+
     def nope(self):
         for rule in self.app.url_map.iter_rules():
             print(rule)
